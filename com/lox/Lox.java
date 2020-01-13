9d5e938a5ea11c13498a9a00c600796a3114b08c
Alkatraz <anuragbmore@gmail.com>
2020-01-13 18:16:06 +0530

    added tokens and token types

--- com/lox/Lox.java
+++ com/lox/Lox.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 
 public class Lox{
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException{
         if(args.length > 1){
             System.out.println("Usage: jlox [script]");
@@ -24,6 +25,7 @@ public class Lox{
     private static void runFile(String path) throws IOException{
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException{
@@ -32,6 +34,7 @@ public class Lox{
         for(;;){
             System.out.println("> ");
             run(reader.readLine());
+            hadError = false;
         }
     }
 
